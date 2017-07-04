ff95923426811e715f44d7ffdc25360422b3a17f
CU7IOUS <garkusha.sergiy@gmail.com>
2017-07-03 23:22:27 -0700

    More features

--- server.py
+++ server.py
@@ -1,14 +1,25 @@
+from os import environ as env
 from sys import argv
 from sanic import Sanic
-# print (sanic.__file__)
+from dotenv import load_dotenv, find_dotenv
+
+print(bool(env.get("DEBUG")))
 
 if __name__ == "__main__":
-    host = "127.0.0.1"
-    port = 9080
+    load_dotenv(find_dotenv())
+
+    host = env.get("HOST") or "localhost"
+    port = int(env.get("PORT")) or 9080
+    workers = int(env.get("WORKERS")) or 1
+    debug = bool(env.get("DEBUG")) or False
+    ssl = env.get("SSL") or None
+    sock = env.get("SOCK") or None
+    loop = env.get("LOOP") or None
 
     if len(argv) > 3:
         host = argv[2]
         port = argv[4]
 
-    app = Sanic()
-    app.run(host, port)
+    app = Sanic(__name__)
+
+    app.run(host, port, debug, ssl, sock, workers, loop)
